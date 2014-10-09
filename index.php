7ca280bbb87c78e547f095778fb0cca505b04811
Christian Amenós <christian.amenos@gmail.com>
2014-10-09 14:45:43 +0200

    New folder strctures skeleton defined, inclusion of external resources

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Music of my life</title>
 
+        <!-- Bootstrap -->
+        <link href="assets/bootstrap/css/bootstrap.min.css"/>
+        <link href="assets/bootstrap/css/bootstrap-theme.min.css"/>
+        <!-- Music of my life -->
+        <link href="assets/css/styles.css"/>
+        
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div id="wrapper">
+            
+        </div>
 
+        <!-- jQuery (http://api.jquery.com/) -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Bootstrap (http://getbootstrap.com/javascript/) -->
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <!-- Timbre (http://mohayonao.github.io/timbre.js/) -->
+        <script src="assets/timbre/timbre.js"></script>
+        <!-- Music of my life -->
+        <script src="assets/js/scripts.js"></script>
+    </body>
+</html>
